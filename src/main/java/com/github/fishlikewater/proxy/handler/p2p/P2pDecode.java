d8359f90d10ff7987182801003aa04d7ebcd7343
fishlikewater@126.com <fishlikewater@126.com>
2019-07-27 10:37:50 +0800

    添加p2p实现

--- src/main/java/com/github/fishlikewater/proxy/handler/p2p/P2pDecode.java
+++ src/main/java/com/github/fishlikewater/proxy/handler/p2p/P2pDecode.java
@@ -1,8 +1,11 @@
 package com.github.fishlikewater.proxy.handler.p2p;
 
+import com.alibaba.fastjson.JSON;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.util.CharsetUtil;
 
 import java.util.List;
 
@@ -16,6 +19,9 @@ public class P2pDecode extends MessageToMessageDecoder<DatagramPacket> {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, DatagramPacket msg, List<Object> out) throws Exception {
-
+        ByteBuf content = msg.content();
+        String s = content.toString(CharsetUtil.UTF_8);
+        P2pMessage p2pMessage = JSON.parseObject(s, P2pMessage.class);
+        out.add(p2pMessage);
     }
 }
--- src/test/java/com/github/fishlikewater/udp/UdpTest.java
+++ src/test/java/com/github/fishlikewater/udp/UdpTest.java
@@ -1,5 +1,8 @@
 package com.github.fishlikewater.udp;
 
+import com.alibaba.fastjson.JSON;
+import com.github.fishlikewater.proxy.handler.p2p.P2pMessage;
+import io.netty.util.CharsetUtil;
 import org.junit.Test;
 
 /**
@@ -15,6 +18,13 @@ public class UdpTest {
 
     @Test
     public void testUdp(){
-        new UdpClient().sendPackage();
+        //new UdpClient().sendPackage();
+        P2pMessage msg = new P2pMessage();
+        msg.setBody("1111");
+        msg.setTargetIp("192.168.0.117");
+        msg.setTargetPort(13455);
+        byte[] content = JSON.toJSONString(msg).getBytes(CharsetUtil.UTF_8);
+        int length =content.length;
+        System.out.println(length);
     }
 }
