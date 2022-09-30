c0d050f4a3dccb37dda6ba5ec1ddd357d1fea076
fish <fishlikewater@126.com>
2022-09-30 16:21:13 +0800

    超时时间

--- src/main/java/com/github/fishlikewater/proxy/conf/ProxyType.java
+++ src/main/java/com/github/fishlikewater/proxy/conf/ProxyType.java
@@ -6,8 +6,7 @@ package com.github.fishlikewater.proxy.conf;
  * @mail fishlikewater@126.com
  * @ClassName ProxyType
  * @Description
- * @Date 2019年03月05日 16:15
- * @since
+ * @since 2019年03月05日 16:15
  **/
 public enum ProxyType{
     http, tcp, socks, p2p, socks_proxy, dns, proxy_server, proxy_server_http, proxy_client, socks_client;
