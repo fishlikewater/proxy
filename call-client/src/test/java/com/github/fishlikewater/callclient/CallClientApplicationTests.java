30ae57280b9c731df8a9b9e425eee4187a89d021
fish <fishlikewater@126.com>
2023-03-14 17:25:49 +0800

    config

--- call-client/src/test/java/com/github/fishlikewater/callclient/CallClientApplicationTests.java
+++ call-client/src/test/java/com/github/fishlikewater/callclient/CallClientApplicationTests.java
@@ -3,11 +3,11 @@ package com.github.fishlikewater.callclient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CallClientApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
---
+++ call-client/src/test/java/com/github/fishlikewater/callclient/JpcapTest.java
@@ -0,0 +1,35 @@
+package com.github.fishlikewater.callclient;
+
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapIf;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author fishlikewater@126.com
+ * @since 2023年03月14日 16:50
+ **/
+public class JpcapTest {
+
+
+    @Test
+    public void testJp(){
+        StringBuilder errsb = new StringBuilder();
+        List<PcapIf> devs = new ArrayList<PcapIf>();
+        int r = Pcap.findAllDevs(devs, errsb);
+        if (r == Pcap.NOT_OK || devs.isEmpty()) {
+            System.err.println("未获取到网卡");
+        } else {
+            System.out.println("获取到网卡：");
+            System.out.println(devs);
+        }
+    }
+
+}
