d43585e69473ea76a14ed596b87d7c2d4a5d0dca
fish <fishlikewater@126.com>
2023-03-24 09:28:22 +0800

    update cut packets

--- cut-packets/src/main/java/com/github/fishlikewater/cutpackets/CutPacketsHelper.java
+++ cut-packets/src/main/java/com/github/fishlikewater/cutpackets/CutPacketsHelper.java
@@ -1,7 +1,9 @@
 package com.github.fishlikewater.cutpackets;
 
 import jpcap.JpcapCaptor;
+import jpcap.JpcapSender;
 import jpcap.NetworkInterface;
+import jpcap.packet.Packet;
 import jpcap.packet.TCPPacket;
 
 import java.io.IOException;
@@ -17,6 +19,8 @@ import java.util.Scanner;
  * @since 2023年03月21日 10:28
  **/
 public class CutPacketsHelper {
+
+    private  JpcapSender jpcapSender;
 
     public void startCutPackets(String ipPrefix, Channel channel){
         NetworkInterface[] devices = JpcapCaptor.getDeviceList();
@@ -34,10 +38,16 @@ public class CutPacketsHelper {
         } while (selectDevice < 0 || selectDevice > devices.length);
         try {
             JpcapCaptor jpcap = JpcapCaptor.openDevice(devices[3], 2000, false, 10000);
+            jpcapSender = jpcap.getJpcapSenderInstance();
             jpcap.setFilter("tcp and dst net " + ipPrefix + "0/24", true);
             jpcap.loopPacket(-1, packet -> {
                 if (packet instanceof TCPPacket){
                     final TCPPacket tcpPacket = (TCPPacket) packet;
+                    final boolean syn = tcpPacket.syn;
+                    final boolean ack = tcpPacket.ack;
+                    if (syn && !ack){
+
+                    }
                     final String hostAddress = tcpPacket.src_ip.getHostAddress();
                     System.out.println(hostAddress);
                 }
@@ -47,8 +57,12 @@ public class CutPacketsHelper {
             e.printStackTrace();
             System.out.println("抓取数据包时出现异常!!");
         }
+    }
 
 
+    public void send(String srcIp, int srcPort){
+        final Packet packet = new Packet();
+        jpcapSender.sendPacket(packet);
     }
 
 
