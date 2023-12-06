319b0b5e6c44afbc53827e6409c4dac092d618d3
zhangxiang <zhangxiang@uphicoo.com>
2023-12-06 15:31:30 +0800

    chore: 打包脚本及windows启动脚本

--- client/bin/start.bat
+++ client/bin/start.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001
 if not exist "%JAVA_HOME%\bin\java.exe" echo Please set the JAVA_HOME variable in your environment, We need java(x64)! jdk8 or later is better! & pause & EXIT
 set "JAVA=%JAVA_HOME%\bin\java.exe"
 
